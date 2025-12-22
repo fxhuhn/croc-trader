ffcf1811147ce7eda00a753f1f8d2468522f6ea2
Markus <github@fxhuhn.de>
2025-12-22 13:37:14 +0100

    switch to env for settings

--- app/config.py
+++ app/config.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
+import os
 from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @dataclass(frozen=True)
 class Config:
     DB_PATH: str = "signals.db"
-    HOST: str = "0.0.0.0"
-    PORT: int = 8000
+    HOST: str = os.getenv("WEBSERVER_HOST", "127.0.0.1")
+    PORT: int = int(os.getenv("WEBSERVER_PORT", "5000"))
     DEBUG: bool = False
