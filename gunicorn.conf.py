9622a09a7e5892315909bf22fe39c33b82629825
Markus <github@fxhuhn.de>
2026-01-07 08:22:45 +0100

    add filter for healrh status 200

---
+++ gunicorn.conf.py
@@ -0,0 +1,17 @@
+# gunicorn_conf.py
+import logging
+
+
+class HealthCheckFilter(logging.Filter):
+    def filter(self, record):
+        # Filtert Zeilen raus, die "GET /health" enthalten
+        return "GET /health" not in record.getMessage()
+
+
+def on_starting(server):
+    """
+    Diese Funktion wird von Gunicorn automatisch beim Start ausgeführt.
+    Wir hängen unseren Filter an den 'gunicorn.access' Logger.
+    """
+    access_logger = logging.getLogger("gunicorn.access")
+    access_logger.addFilter(HealthCheckFilter())
