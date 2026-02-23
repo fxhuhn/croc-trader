8c0ddb78248c5678860a965fb255175fc58dd4c4
Markus <github@fxhuhn.de>
2026-02-23 20:46:06 +0100

    add honey pot

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from .api import api_blueprint
 from .errors import errors_bp
 from .views import views_bp
+from .honeypot import honeypot_bp
 
 # Haupt-Blueprint, das von der App registriert wird
 main_bp = Blueprint("main_aggregator", __name__)
@@ -12,3 +13,5 @@ main_bp = Blueprint("main_aggregator", __name__)
 main_bp.register_blueprint(api_blueprint)
 main_bp.register_blueprint(views_bp)
 main_bp.register_blueprint(errors_bp)
+main_bp.register_blueprint(honeypot_bp)
+
